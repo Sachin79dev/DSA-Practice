529be455ae2e0b1bf14b0d5f007c20d4274bdffb
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-07 17:16:03 +0530

    Count vowels and consonants in a given string
    Fix counting logic for consonants and vowels.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 
+// counting the vowels inside given string
+
+
+let str = prompt("Enter a String")
+let consonent = 0;
+let vowels = 0;
+
+for(let i = 0; i<str.length; i++) {
+    ch = str.charAt(i)
+
+    switch(ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u': {
+            vowels++
+            break
+        }
+
+        default : {
+            consonent++ 
+        }
+    }
+}
+
+console.log(`Consonent are ${vowels}`);
+console.log(`Vowels are ${vowels}`);
