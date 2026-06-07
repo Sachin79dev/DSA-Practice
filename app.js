5d8882d31217116afa21700e70a6f58950d79014
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-07 18:38:23 +0530

    Add function to print day name from day number
    Add function to print day name from day number

--- app.js
+++ app.js
@@ -107,3 +107,33 @@ function findGreatest(a, b, c) {
 }
 
 findGreatest(3, 4, 5)
+
+
+
+// Print Day name from Day number 
+
+
+function printDayName(day) {
+    //write your code here
+    switch (day) {
+        case 1:
+            return 'Monday';
+        case 2:
+            return 'Tuesday';
+        case 3:
+            return 'Wednesday';
+        case 4:
+            return 'Thursday';
+        case 5:
+            return 'Friday';
+        case 6:
+            return 'Saturday';
+        case 7:
+            return 'Sunday';
+
+        default:
+            return 'Invalid day number'
+    }
+}
+
+console.log(printDayName(3)); // Wednesday
