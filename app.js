246420b371b25295a657469edab07c9887f0523b
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-07 18:13:07 +0530

    Implement area calculation for shapes using switch
    Added functionality to calculate the area of a circle, rectangle, and triangle using a switch statement based on user input.

--- app.js
+++ app.js
@@ -48,3 +48,43 @@ for(let i = 0; i<str.length; i++) {
 }
 console.log(`Vowels are ${vowels}`);
 console.log(`Consonant are ${cons}`);
+
+
+
+
+
+// calcultaing the area of circle, rectangle and triangle using switch case constioning 
+
+// circle -- (A=πr2) // radius of circle = r.
+// rectangle -- (A=lw) // length = l, width = w.
+// triangle -- (A=1/2​bh) // base = b, height = h.
+
+
+let whichArea = Number(prompt())
+
+
+switch(whichArea) {
+    case 1 : {
+        const pi = 3.14;
+        let radius = Number(prompt("Enter radius of circle"));
+        console.log("Area of Circle =", pi*(radius*radius));
+        break;
+    }
+
+    case 2 : {
+        let length = Number(prompt("Enter length of rectangle"));
+        let width = Number(prompt("Enter width of rectangle"));
+        console.log("Area of Rectangle =", length*width);
+        break;
+    }
+
+    case 3 : {
+        
+        let base = Number(prompt("Enter base of triangle"));
+        let height = Number(prompt("Enter height of triangle"));
+        console.log("Area of Triangle =", 0.5*(base*height));
+        break;
+    }
+
+    default: console.log("Invalid Number");  
+}
