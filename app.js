82a1fad3825793a47d4f0a134878b40236483631
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-08 17:31:52 +0530

    Add function to sum numbers up to n
    Add function to sum numbers up to n

--- app.js
+++ app.js
@@ -199,3 +199,24 @@ function printNumbers(n) {
 
 let ans = printNumbers(5);
 console.log(ans.join(' '));
+
+
+
+
+
+// Sum upto n term 
+
+
+function sumUpToN(n) {
+    let sum = 0;
+    
+    for(i = 1; i <= n; i++){
+        sum += i
+    }
+
+    return sum
+}
+
+let sumed = sumUpToN(5)
+
+console.log(sumed);
