6496e8b7d0404cf0be57a1c78977781c84241006
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-23 18:09:48 +0530

    Add factorial function to calculate factorials
    Add factorial function to calculate factorials

--- app.js
+++ app.js
@@ -1555,3 +1555,26 @@ console.log(login("Sachin", "1234")); // Invalid Credentials
 
 
 
+
+//  Factorial Function 
+
+
+function factorial(n) {
+    let fact = 1;
+    for(let i = 1; i <= n; i++) {
+        fact *= i; 
+    }
+
+    return fact
+    
+}
+
+
+console.log(factorial(5)); // 120 
+
+
+
+
+
+
+
